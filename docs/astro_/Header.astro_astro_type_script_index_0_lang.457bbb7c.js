4652bfea5b45c91bee489a27e149259d5f945efa
Lexikus <alexander.bifulco@gmail.com>
2023-05-21 17:12:59 +0200

    feat: add burger for nav, prepare wishing list

---
+++ docs/astro_/Header.astro_astro_type_script_index_0_lang.457bbb7c.js
@@ -0,0 +1,1 @@
+const t=document.getElementById("burger"),s=document.getElementById("nav-list");let e=!1;t?.addEventListener("click",i=>{e?s?.classList.remove("visible"):s?.classList.add("visible"),e=!e});
